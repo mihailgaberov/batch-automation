76b5cebf2c458c459256a44ddce389a64ba01c08
Mihail Gaberov <mihail.gaberov@gmail.com>
2018-06-22 13:49:45 +0200

    Create startide.bat

---
+++ startide.bat
@@ -0,0 +1,8 @@
+@echo off
+title IntelliJ IDEA Starter
+echo. && echo Starting IDE...
+start "" "C:\Program Files (x86)\JetBrains\IntelliJ IDEA Ultimate 17.2.2\bin\idea64.exe"
+pause
+echo Done!
+pause
+exit
