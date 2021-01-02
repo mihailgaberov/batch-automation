a5bebd4c915abc2c2b18edd8332f98d28d1ab727
Mihail Gaberov <mihail.gaberov@gmail.com>
2021-01-02 10:40:40 +0100

    Create start_chrome_with_devtools.bat

---
+++ start_chrome_with_devtools.bat
@@ -0,0 +1,4 @@
+echo Opening Chrome...
+start "" "path/to/chrome.exe" --auto-open-devtools-for-tabs <URL>
+e.g.
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --auto-open-devtools-for-tabs https://mihail-gaberov.eu
