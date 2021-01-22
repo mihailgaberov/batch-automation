ded7d304859383a711bec50a8c238f05a43061b7
Mihail Gaberov <mihail.gaberov@gmail.com>
2021-01-22 17:44:54 +0100

    Update merge_and_start.bat

--- merge_and_start.bat
+++ merge_and_start.bat
@@ -1,21 +1,21 @@
-echo "-> Routing to project root"
+echo Routing to project root . . . 
 cd /
 cd \path\to\project\root
 pause
-echo "-> Pulling current branch changes from Git"
+echo Pulling current branch changes from Git . . .
 git pull
 pause
-echo "-> Checking out `dev` branch"
-git checkout -b dev
+echo Checking out `dev` branch . . .
+git checkout dev
 pause
-echo "-> Pulling `dev` branch changes from Git"
+echo Pulling `dev` branch changes from Git . . .
 git pull
 pause
-echo "-> Checking out previous branch"
+echo Checking out previous branch . . .
 git checkout @{-1}
 pause
-echo "-> Merging `dev` into current branch"
+echo Merging `dev` into current branch . . .
 git merge dev
 pause
-echo "-> Starting the project via NPM"
+echo Starting the project via NPM . . .
 npm start
